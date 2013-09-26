e65d905dcd2d4ad7de77290721673767299e4f59
andrey <evandvlad@gmail.com>
2013-09-26 19:01:59 +0400

    small optimization

--- tmpl.js
+++ tmpl.js
@@ -8,19 +8,15 @@
 
     'use strict';
 
-    global.tmpl = (function(){
-        var proc = function(all, g1){
-            var s = g1.replace(/\\'/g, "'");
-            return s.charAt(0) === '=' ? ("'+" + s.slice(1) + "+'") : ("';" + s + "s+='");
-        };
-
-        return function(str){
-            return new Function("data", "var s='" +
-                str.replace(/[\r\t\n]/g, " ")
-                    .replace(/'/g, "\\'")
-                    .replace(/<#(.+?)#>/g, proc)
-                + "';return s;");
-        };
-    })();
+    global.tmpl = function(str){
+        return new Function("data", "var s='" +
+            str.replace(/[\r\t\n]/g, " ")
+               .replace(/'/g, "\\'")
+               .replace(/<#(.+?)#>/g, function(all, g1){
+                   var s = g1.replace(/\\'/g, "'");
+                   return s.charAt(0) === '=' ? ("'+" + s.slice(1) + "+'") : ("';" + s + "s+='");
+               })
+            + "';return s;");
+    };
 
 }(this));
